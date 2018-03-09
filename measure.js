2a6da2d69935a5c28ca5ace60e79edd13ebf928d
asusventura123 <jacs501@york.ac.uk>
2018-03-09 12:39:40 +0000

    resized panels and removed animations

--- measure.js
+++ measure.js
@@ -5,22 +5,25 @@ $(document).ready(function() {
     
     var unit = 'V'
     var range = 'm'
-    var hold = false;
+    var hold = false
+
+    var runInt
+
     function menu() {
 
         $(".menu").hide()
 
         $(".toggleMenu").on('click', () => {
-            $(".menu").slideToggle(1500)
-            $(".modemenu").hide(300)
-            $(".rangemenu").hide(300)
+            $(".menu").slideToggle()
+            $(".modemenu").hide()
+            $(".rangemenu").hide()
             $("#mode").on('click', function() {
-                $('.modemenu').toggle(1000)
+                $('.modemenu').toggle()
                 $('.rangemenu').hide()
             })
             $("#range").on('click', function() {
                 $('.modemenu').hide()
-                $('.rangemenu').toggle(1000)
+                $('.rangemenu').toggle()
             })
             $("#screenshot").on('click', () => {
                 alert("Are you sure you want to take a screenshot?")
@@ -32,20 +35,20 @@ $(document).ready(function() {
     function getMode() {
         $("#voltage").on('click', function(e) {
             unit = 'V'
-            $('.modemenu').toggle(300)
-            $('.menu').toggle(300)
+            $('.modemenu').toggle()
+            $('.menu').toggle()
             $("h1 #unitvalue").text(unit)
         })
         $("#current").on('click', function(e) {
             unit = 'A'
-            $('.modemenu').toggle(300)
-            $('.menu').toggle(300)
+            $('.modemenu').toggle()
+            $('.menu').toggle()
             $("h1 #unitvalue").text(unit)
         })
         $("#resistance").on('click', function(e) {
             unit = 'R'
-            $('.modemenu').toggle(300)
-            $('.menu').toggle(300)
+            $('.modemenu').toggle()
+            $('.menu').toggle()
             $("h1 #unitvalue").text(unit)
         })
         
@@ -54,44 +57,44 @@ $(document).ready(function() {
     function getRange() {
         $('#mega').on('click', function(e) {
             range = 'M'
-            $('.rangemenu').toggle(300)
-            $('.menu').toggle(300)
+            $('.rangemenu').toggle()
+            $('.menu').toggle()
             $("h1 #rangevalue").text(range)
         })
         $('#kilo').on('click', function(e) {
             range = 'k'
-            $('.rangemenu').toggle(300)
-            $('.menu').toggle(300)
+            $('.rangemenu').toggle()
+            $('.menu').toggle()
             $("h1 #rangevalue").text(range)
         })
         $('#none').on('click', function(e) {
             range = ''
-            $('.rangemenu').toggle(300)
-            $('.menu').toggle(300)
+            $('.rangemenu').toggle()
+            $('.menu').toggle()
             $("h1 #rangevalue").text(range)
         })
         $('#milli').on('click', function(e) {
             range = 'm'
-            $('.rangemenu').toggle(300)
-            $('.menu').toggle(300)
+            $('.rangemenu').toggle()
+            $('.menu').toggle()
             $("h1 #rangevalue").text(range)
         })
         $('#micro').on('click', function(e) {
             range = 'u'
-            $('.rangemenu').toggle(300)
-            $('.menu').toggle(300)
+            $('.rangemenu').toggle()
+            $('.menu').toggle()
             $("h1 #rangevalue").text(range)
         })
         $('#nano').on('click', function(e) {
             range = 'n'
-            $('.rangemenu').toggle(300)
-            $('.menu').toggle(300)
+            $('.rangemenu').toggle()
+            $('.menu').toggle()
             $("h1 #rangevalue").text(range)
         })
         $('#pico').on('click', function(e) {
             range = 'p'
-            $('.rangemenu').toggle(300)
-            $('.menu').toggle(300)
+            $('.rangemenu').toggle()
+            $('.menu').toggle()
             $("h1 #rangevalue").text(range)
         })
 
@@ -100,14 +103,14 @@ $(document).ready(function() {
     function turnOff() {
         $("#off").on('click', () => {
             if(confirm("Are you sure you want to exit the application?")){
-                win.close();
+                win.close()
                 }
             })
     }
 
     
     function display() {
-        var i = Math.random() * 12;
+        var i = Math.random() * 12
         i = Math.round(i * 1000) / 1000
             
         $("h1 #value").text(i)
@@ -130,31 +133,24 @@ $(document).ready(function() {
         return "MEASure:"+mode
     }
 
-    function holdDisplay(displayID) {
-        $('#hold').on('click', function(e) {
-            if (hold) {
-                hold = false
-                clearInterval(displayID)
-                var newDisplay = setInterval(function(){display()}, 1000)
-                return newDisplay
-            }
-            else {
-                hold = true
-                clearInterval(displayID)
-                return null
-            }
-            console.log(hold)
-        })
+
+    $('#hold').on('click', function(e) {
+        if (hold) {
+            hold = false
+            runInt = setInterval(display, 1000)
+        }
+        else {
+            hold = true
+            clearInterval(runInt)
+        }
+    })
         
-    }
+    runInt = setInterval(display, 1000)
 
-
-    var runDisplay = setInterval(function(){display()}, 1000)
     turnOff()
     menu()
     getMode()
     getRange()
     setInterval(function(){msg2Board(unit)}, 1000)
-    runDisplay = holdDisplay(runDisplay)
 
 })
