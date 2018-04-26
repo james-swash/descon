5333c9313f7b8c44cb0befda97663ad80071eb34
Chris Taylor <christhebaron@gmail.com>
2018-04-26 14:36:28 +0100

    Trying something with continuity

--- measure.js
+++ measure.js
@@ -44,7 +44,7 @@ function getMode() {
         $(".rangemenu #zero").text('-10 to 10 V')
         $(".rangemenu #one").text('-1 to 1 V')
         $(".rangemenu #two").text('-100 to 100 mV')
-        $("#continuity").text("")
+        $("#continuity").hide();
         switch(rangeVal) {
             case 0:
                 $("h1 #rangevalue").text('')
@@ -74,7 +74,7 @@ function getMode() {
         $(".rangemenu #zero").text('-10 to 10 V')
         $(".rangemenu #one").text('-1 to 1 V')
         $(".rangemenu #two").text('-100 to 100 mV')
-        $("#continuity").text("")
+        $("#continuity").hide();
         switch(rangeVal) {
             case 0:
                 $("h1 #rangevalue").text('')
@@ -104,7 +104,7 @@ function getMode() {
         $(".rangemenu #zero").text('-1 to 1 A')
         $(".rangemenu #one").text('-100 to 100 mA')
         $(".rangemenu #two").text('-10 to 10 mA')
-        $("#continuity").text("")
+        $("#continuity").hide();
         switch(rangeVal) {
             case 0:
                 $("h1 #rangevalue").text('m')
@@ -134,7 +134,7 @@ function getMode() {
         $(".rangemenu #zero").text('10 to 10k Ohm')
         $(".rangemenu #one").text('100 to 100k Ohm')
         $(".rangemenu #two").text('1k to 1M Ohm')
-        $("#continuity").text("Continuity: Off")
+        $("#continuity").show();
         switch(rangeVal) {
             case 0:
                 $("h1 #rangevalue").text('')
@@ -265,19 +265,25 @@ function getAudio() {
     })
 }
 
-function getContinuity() {
-    $('#continuity').on('click', function(e){
-        if(unit === 'R'){
-            if ($("#continuity").text() === "Continuity: Off"){
-                commandMsg = 'CONTinuity: On'
-                $("#continuity").text("Continuity: On")
-            }
-            else {
+function addContinuityHandler() {
+
+    let cont = false;
+
+    $('#continuity').on('click', function (e) {
+        if (unit === 'R') {
+            if (cont) {
                 commandMsg = 'CONTinuity: Off'
                 $("#continuity").text("Continuity: Off")
+                cont = false;
+            }
+            else {
+                commandMsg = 'CONTinuity: On'
+                $("#continuity").text("Continuity: On")
+                cont = true;
             }
         }
     })
+
 }
 
 function screenShot() {
@@ -419,6 +425,7 @@ $(function() {
     menu()
     getMode()
     getAudio()
+    addContinuityHandler()
     // getUnitType()
     getRange()
     screenShot()
