bb4710835d787c5003974e26658a0ccb84c1ca7a
asusventura123 <jacs501@york.ac.uk>
2018-04-27 15:20:40 +0100

    Committed

--- temp_humid.py
+++ temp_humid.py
@@ -12,11 +12,11 @@ sensor = BMP085.BMP085()
 while True:
 	contents = "["
 	humidity, temperature = Adafruit_DHT.read_retry(11, 4)
-	contents += '"{0:0.2f}*C", '.format(sensor.read_temperature()) #Temp
-	contents += '"{0:0.2f}Pa", '.format(sensor.read_pressure()) #Pressure
-	contents += '"{0:0.2f}m", '.format(sensor.read_altitude()) #Altitude
-	contents += '"{0:0.2f}Pa", '.format(sensor.read_sealevel_pressure()) #sealevel pressure
-	contents += '"{0:0.1f}%"]'.format(humidity) # humidity
+	contents += '"{0:0.2f}", '.format(sensor.read_temperature()) #Temp
+	contents += '"{0:0.2f}", '.format(sensor.read_pressure()) #Pressure
+	contents += '"{0:0.2f}", '.format(sensor.read_altitude()) #Altitude
+	contents += '"{0:0.2f}", '.format(sensor.read_sealevel_pressure()) #sealevel pressure
+	contents += '"{0:0.1f}"]'.format(humidity) # humidity
 	temp_file = open('../temperature.txt', 'w')
 	print(contents)
 	temp_file.write(contents)
